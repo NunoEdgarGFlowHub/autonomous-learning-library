335d1f72cc9865b61273115faefcb6f2049708ac
Chris Nota <cpnota@gmail.com>
2019-06-15 13:32:40 -0400

    reenable multi-dim q tests

--- all/approximation/multi_q_network_test.py
+++ all/approximation/multi_q_network_test.py
@@ -1,9 +1,7 @@
 import unittest
 import torch
 from torch import nn
-from torch.nn.functional import smooth_l1_loss
 import torch_testing as tt
-import numpy as np
 from all.environments import State
 from all.approximation.q_network import QNetwork
 
@@ -31,47 +29,47 @@ class TestMultiDimQNetwork(unittest.TestCase):
         optimizer = torch.optim.SGD(self.model.parameters(), lr=0.1)
         self.q = QNetwork(self.model, optimizer, ACTIONS)
 
-    # def test_forward(self):
-    #     state = State(torch.randn(2, STATE_DIM))
-    #     results = self.q(state)
-    #     expected = torch.tensor([
-    #         [
-    #             [-0.2371,  0.4967],
-    #             [-0.0580,  0.2703],
-    #             [ 0.1734, -0.4500]
-    #         ], [
-    #             [-0.0341,  0.5139],
-    #             [-0.3381,  1.0650],
-    #             [ 1.0020, -0.5296]
-    #         ]
-    #     ])
-    #     tt.assert_almost_equal(results, expected, decimal=3)
+    def test_forward(self):
+        state = State(torch.randn(2, STATE_DIM))
+        results = self.q(state)
+        expected = torch.tensor([
+            [
+                [-0.2371,  0.4967],
+                [-0.0580,  0.2703],
+                [ 0.1734, -0.4500]
+            ], [
+                [-0.0341,  0.5139],
+                [-0.3381,  1.0650],
+                [ 1.0020, -0.5296]
+            ]
+        ])
+        tt.assert_almost_equal(results, expected, decimal=3)
 
-    # def test_mask(self):
-    #     state = State(torch.randn(2, STATE_DIM), torch.tensor([0, 1]))
-    #     results = self.q(state)
-    #     expected = torch.tensor([
-    #         [
-    #             [0, 0],
-    #             [0, 0],
-    #             [0, 0]
-    #         ], [
-    #             [-0.0341,  0.5139],
-    #             [-0.3381,  1.0650],
-    #             [ 1.0020, -0.5296]
-    #         ]
-    #     ])
-    #     tt.assert_almost_equal(results, expected, decimal=3)
+    def test_mask(self):
+        state = State(torch.randn(2, STATE_DIM), torch.tensor([0, 1]))
+        results = self.q(state)
+        expected = torch.tensor([
+            [
+                [0, 0],
+                [0, 0],
+                [0, 0]
+            ], [
+                [-0.0341,  0.5139],
+                [-0.3381,  1.0650],
+                [ 1.0020, -0.5296]
+            ]
+        ])
+        tt.assert_almost_equal(results, expected, decimal=3)
 
-    # def test_choose_action(self):
-    #     state = State(torch.randn(2, STATE_DIM))
-    #     actions = torch.tensor([1, 2])
-    #     results = self.q(state, actions)
-    #     expected = torch.tensor([
-    #         [-0.0580,  0.2703],
-    #         [ 1.0020, -0.5296]
-    #     ])
-    #     tt.assert_almost_equal(results, expected, decimal=3)
+    def test_choose_action(self):
+        state = State(torch.randn(2, STATE_DIM))
+        actions = torch.tensor([1, 2])
+        results = self.q(state, actions)
+        expected = torch.tensor([
+            [-0.0580,  0.2703],
+            [ 1.0020, -0.5296]
+        ])
+        tt.assert_almost_equal(results, expected, decimal=3)
 
     def test_backwards(self):
         # generate initial output
